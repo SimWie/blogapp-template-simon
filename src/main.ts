39bcb30b5bd7abe51a4bacbef2c163b16887a1be
Simon Wiedmer <194578540+SimWie@users.noreply.github.com>
2026-05-30 11:21:48 +0200

    test: depoyTest2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-
-//Test comment to trigger CI/CD pipeline
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+//Test comment to trigger CI/CD pipeline Test 2
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+//Test comment to trigger CI/CD pipeline Test 3
