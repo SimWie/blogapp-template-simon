65e23a495c20e6f493627bb6eeee675dac1539e0
Simon Wiedmer <194578540+SimWie@users.noreply.github.com>
2026-05-30 10:52:47 +0200

    testCommit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+
+//Test comment to trigger CI/CD pipeline
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
